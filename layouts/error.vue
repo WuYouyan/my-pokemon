3835c0a8afd34ecaa9c970e3fe2147240085908e
Youyan WU <wuyouyan1@gmail.com>
2020-05-20 19:59:58 +0200

    [ADD] Add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h2 v-if="error.statusCode==404">404 page lost</h2>
+      <h2 v-else>500 Server error </h2>
+      <ul>
+          <li><nuxt-link to="/">HOME</nuxt-link></li>
+      </ul>
+  </div>
+</template>
+ 
+<script>
+export default {
+  props:['error'],
+}
+</script>

d84213514d516fbaa391128c8e0e6127d0cb9f8d
Youyan WU <wuyouyan1@gmail.com>
2020-05-20 18:38:33 +0200

    [EVO] Pokemon List (Basic)

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,87 @@
 <template>
-  <div class="container">
+  <div class="Main">
     <div>
-      <logo />
+      <!-- <logo /> -->
       <h1 class="title">
-        my-pokemon
-      </h1>
-      <h2 class="subtitle">
-        My peachy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        Pokemon List
+      </h1>   
+    </div>
+    <div class="container">
+      <ul>
+        <li v-for="pokemon in pokemonList" :key="pokemon.name">{{pokemon.name}}</li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import axios from 'axios';
 
-export default Vue.extend({
-  components: {
-    Logo
+const PATH_BASE = 'https://pokeapi.co/api/v2/';
+const LIMIT = 'limit=';
+
+const getPokemonData$ = (url:string): Promise<any> => {
+  return axios.get(url);
+}
+
+const getPokemons$ = (listNumber: number): Promise<any[]> => 
+  axios.get(`${PATH_BASE}pokemon?${LIMIT}${listNumber}`).then(
+    res => {
+      let pkList = res.data.results; 
+      // for (let index = 0; index < pkList.length; index++) {
+      //   getPokemonData$(pkList[index].url).then(res =>
+      //     pkList[index].sprites = res.data.sprites
+      //   );
+      // }
+      pkList.map((pk:any) =>
+        getPokemonData$(pk.url).then(res =>
+          pk.sprites = res.data.sprites
+        )
+      )
+      return pkList;
+    } 
+  )
+
+
+export default {
+
+  // asyncData () {
+  //   return getPokemons$(2).then(
+  //     (res) => {
+  //       console.log('res: ', res);
+  //       return { pokemonList: res }
+  //     }
+  //   );
+  // }
+  async asyncData () {
+    const result = await getPokemons$(50);
+    return { pokemonList: result }
   }
-})
+}
 </script>
 
 <style>
+.main {
+  margin: 0 ;
+  padding: 10em ;
+  background: #f59944;
+}
+
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-  text-align: center;
 }
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
+  text-align: center;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 8vh;
   color: #35495e;
   letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
